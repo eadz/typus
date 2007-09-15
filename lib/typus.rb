c6368cfd45611ff5f9eec491d82d7800feb85ab1
Francesc Esplugas <fesplugas@intraducibles.net>
2007-09-15 20:02:23 +0200

    Check if the setup file exists

--- lib/typus.rb
+++ lib/typus.rb
@@ -5,12 +5,6 @@ def generate_password(length)
   return newpass
 end
 
-TYPUS = YAML.load_file("#{RAILS_ROOT}/config/setup.yml")
-
-#TYPUS[:licenses] = [["Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License", "license-1"],
-#                    ["License 2", "license-2"],
-#                    ["All Rights Reserved", "all-rights-reserved"]]
-#TYPUS[:text_filters] = [['<None>', "none"],
-#                        ['Textile', "textile"],
-#                        ['Markdown', "markdown"],
-#                        ["Test", "test"]]
+if File.exists? "#{RAILS_ROOT}/config/setup.yml"
+  TYPUS = YAML.load_file("#{RAILS_ROOT}/config/setup.yml")
+end
