c9ee517d9cbedf437bb0a06d94e096d49563aafc
Francesc Esplugas <fesplugas@intraducibles.net>
2007-11-12 01:04:37 +0100

    Removed all files ...

--- lib/typus.rb
+++
@@ -1,6 +0,0 @@
-def generate_password(length)
-  chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-  newpass = ""
-  1.upto(length) { |i| newpass << chars[rand(chars.size-1)] }
-  return newpass
-end
--- tasks/royale.rb
+++
@@ -1,9 +0,0 @@
-def load_data
-  @royale_file = File.join(File.dirname(__FILE__), 'royale.yml')
-  @royale = File.open(@royale_file) { |file| YAML::load(file) }
-end
-
-def timestamp
-  Time.now.utc.strftime("%Y%m%d%H%M")
-end
-
