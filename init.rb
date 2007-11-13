1b6f045682cede0bf7c7d183137e814445d1d88c
Francesc Esplugas <fesplugas@intraducibles.net>
2007-11-13 15:59:03 +0100

    Updated init.rb. Added sha1 requirement

--- init.rb
+++ init.rb
@@ -2,5 +2,8 @@
 ActionController::Base.view_paths << File.join(File.dirname(__FILE__), 'app', 'views')
 %w( controllers models helpers ).each { |m| Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', m) }
 
+# Load gems
+%w( sha1 ).each { |lib| require lib }
+
 # Libraries required
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each { |lib| require lib }
