6f71cb5192276915f6372a31996d469ce1d992db
Francesc Esplugas <fesplugas@intraducibles.net>
2007-11-20 12:18:05 +0100

    Fixed -typus_form_externals- Didn't close the form tag

--- app/helpers/typus_helper.rb
+++ app/helpers/typus_helper.rb
@@ -215,6 +215,7 @@ module TypusHelper
       @block += "<p>"
       @block += select "model_id_to_relate", :related_id, (model_to_relate.find(:all) - @item.send(field[0])).map { |f| [f.name, f.id] }
       @block += "&nbsp; #{submit_tag "Add #{field[0].singularize}"}</p>"
+      @block += "</form>"
       current_model = eval params[:model].singularize.capitalize
       items = current_model.find(params[:id]).send(field[0])
       @block += "<ul>"
