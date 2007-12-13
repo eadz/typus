b7a570bec761bb7fefb90297672aaad7c17423f2
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-13 13:00:19 +0100

    Updated README file

--- install.rb
+++ install.rb
@@ -1,1 +1,6 @@
-# Install hook code here
+require 'fileutils'
+
+css = File.dirname(__FILE__) + '/../../../public/stylesheets/typus.css'
+FileUtils.cp File.dirname(__FILE__) + '/app/public/stylesheets/typus.css', css unless File.exist?(css)
+
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
