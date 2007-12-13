87906355023be8802705d6b02cbbc5f071a65ae8
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-13 13:14:51 +0100

    Updated DOC

--- install.rb
+++ install.rb
@@ -3,4 +3,8 @@ require 'fileutils'
 css = File.dirname(__FILE__) + '/../../../public/stylesheets/typus.css'
 FileUtils.cp File.dirname(__FILE__) + '/app/public/stylesheets/typus.css', css unless File.exist?(css)
 
+# Images
+# images_folder = File.dirname(__FILE__) + '/../../../public/images/'
+# system "cp #{RAILS_ROOT}/vendor/plugins/typus/public/#{folder}/* #{RAILS_ROOT}/public/#{folder}/"
+
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
