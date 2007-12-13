60e2c50a0a2536a6e40b891dc396eb315f19f7bd
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-13 13:38:02 +0100

    Updated install script

--- install.rb
+++ install.rb
@@ -1,10 +1,12 @@
 require 'fileutils'
 
+# Copy the stylesheets and images.
 css = File.dirname(__FILE__) + '/../../../public/stylesheets/typus.css'
 FileUtils.cp File.dirname(__FILE__) + '/app/public/stylesheets/typus.css', css unless File.exist?(css)
-
-# Images
 # images_folder = File.dirname(__FILE__) + '/../../../public/images/'
 # system "cp #{RAILS_ROOT}/vendor/plugins/typus/public/#{folder}/* #{RAILS_ROOT}/public/#{folder}/"
 
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+# Check `will_paginate` plugin is installed, otherwise we install it.
+
+# And finally print a message with the options
+puts "rake typus:config (Installs the `typus.yml` configuration file.)"
