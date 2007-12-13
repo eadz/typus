846836350d2289d93c92b9092e2c9eba6b9aa751
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-13 13:38:24 +0100

    Added initial test structure ...

--- test/typus_test.rb
+++ test/typus_test.rb
@@ -1,8 +1,29 @@
 require 'test/unit'
 
 class TypusTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+
+  def test_should_render_index
+    assert true
   end
+
+  def test_should_render_edit
+    assert true
+  end
+
+  def test_should_run_a_filter
+    assert true
+  end
+
+  def test_should_return_flash_after_updating_record
+    assert true
+  end
+
+  def test_should_check_user_is_authenticated
+    assert true
+  end
+
+  def test_should_render_dashboard
+    assert true
+  end
+
 end
