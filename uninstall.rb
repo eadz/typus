c1ec4971a2341a41b261044f3d5ccd114b065948
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-16 20:30:51 +0100

    Updated uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -1,3 +1,11 @@
-# Remove `typus.yml`, stylesheets and images ...
 
-puts "Successfully uninstalled plugin ..."
+puts "\n------------------------------------------------------------------------"
+puts "   Uninstalling Typus"
+puts "------------------------------------------------------------------------\n\n"
+
+%w( stylesheets images ).each do |f|
+  puts " => Removing #{f.capitalize}"
+  system "rm #{File.dirname(__FILE__)}/../../../public/#{f}/typus*"
+end
+
+puts "Successfully uninstalled Typus ..."
