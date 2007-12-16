c3e563465c085c706d097ad3ceb197ef6bb22cb9
Francesc Esplugas <fesplugas@intraducibles.net>
2007-12-16 20:34:45 +0100

    Updated install and uninstall scripts

--- install.rb
+++ install.rb
@@ -1,19 +1,19 @@
 puts "\n------------------------------------------------------------------------"
-puts "   Installing Typus"
+puts "  Installing Typus"
 puts "------------------------------------------------------------------------\n\n"
 
 %w( stylesheets images ).each do |f|
-  puts " => Copying #{f.capitalize}"
+  puts "  => Copying #{f.capitalize}"
   system "cp #{File.dirname(__FILE__)}/public/#{f}/* #{File.dirname(__FILE__)}/../../../public/#{f}/"
 end
 
 puts "\n------------------------------------------------------------------------"
-puts "   Available Tasks"
+puts "  Available Tasks"
 puts "------------------------------------------------------------------------\n\n"
 
-puts " - rake typus:dependencies"
-puts "   Installs +will_paginate+ plugin"
+puts "  - rake typus:dependencies"
+puts "    Installs +will_paginate+ plugin"
 puts ""
-puts " - rake typus:config"
-puts "   Generates +config/typus.yml+ config file."
+puts "  - rake typus:config"
+puts "    Generates +config/typus.yml+ config file."
 puts ""
--- uninstall.rb
+++ uninstall.rb
@@ -1,11 +1,11 @@
 
 puts "\n------------------------------------------------------------------------"
-puts "   Uninstalling Typus"
+puts "  Uninstalling Typus"
 puts "------------------------------------------------------------------------\n\n"
 
 %w( stylesheets images ).each do |f|
-  puts " => Removing #{f.capitalize}"
+  puts "  => Removing #{f.capitalize}"
   system "rm #{File.dirname(__FILE__)}/../../../public/#{f}/typus*"
 end
 
-puts "Successfully uninstalled Typus ..."
+puts "\n  Successfully uninstalled Typus.\n\n"
