7aaf75befd97a688a99a8c78466f172128763880
Francesc Esplugas <fesplugas@intraducibles.net>
2008-02-10 12:25:17 +0100

    Cleaned up uninstall script

--- uninstall.rb
+++ uninstall.rb
@@ -1,11 +1,9 @@
-
-puts "\n------------------------------------------------------------------------"
-puts "  Uninstalling Typus"
-puts "------------------------------------------------------------------------\n\n"
+puts "\nUninstalling Typus"
+puts "=================="
 
 %w( stylesheets images ).each do |f|
-  puts "  => Removing #{f.capitalize}"
+  puts "=> Removing #{f.capitalize}"
   system "rm #{File.dirname(__FILE__)}/../../../public/#{f}/typus*"
 end
 
-puts "\n  Successfully uninstalled Typus.\n\n"
+puts "\n"
