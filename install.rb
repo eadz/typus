b70020c5d06d3620fe6a4ec24adec369fde9c3d1
Francesc Esplugas <fesplugas@intraducibles.net>
2008-03-03 09:58:11 +0100

    Updated install

--- install.rb
+++ install.rb
@@ -1,12 +1,6 @@
-puts "\nInstalling Typus"
-puts "================="
-
 %w( stylesheets images ).each do |f|
-  puts "#{f.capitalize} successfully copied."
   system "cp #{File.dirname(__FILE__)}/public/#{f}/* #{File.dirname(__FILE__)}/../../../public/#{f}/"
 end
 
-puts "\nAvailable Tasks"
-puts "================"
-system "rake -T typus --silent"
-puts "\n"
+puts "[Typus] Available Tasks"
+system "rake typus"
