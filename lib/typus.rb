b2ba472ffc01ff2ffdf77863e6e3b04af7e6adb5
Francesc Esplugas <fesplugas@intraducibles.net>
2008-03-09 21:18:06 +0100

    Added messages, renamed enable_ar to enable_orm and updated method

--- lib/typus.rb
+++ lib/typus.rb
@@ -4,25 +4,34 @@ module Typus
 
     def enable
       enable_configuration
-      enable_activerecord
+      enable_orm
       enable_routes
       enable_string
     end
 
     def enable_configuration
       require 'typus/configuration'
+      puts "=> [TYPUS] Loaded Configuration File"
     end
 
-    def enable_activerecord
-      require 'typus/active_record'
+    def enable_orm
+      if defined?(DataMapper)
+        require 'typus/data_mapper'
+        puts "=> [TYPUS] Loaded DataMapper"
+      else
+        require 'typus/active_record'
+        puts "=> [TYPUS] Loaded ActiveRecord"
+      end
     end
 
     def enable_routes
       require 'typus/routes'
+      puts "=> [TYPUS] Loaded Routes"
     end
 
     def enable_string
       require 'typus/string'
+      puts "=> [TYPUS] Loaded String Extension"
     end
 
   end
