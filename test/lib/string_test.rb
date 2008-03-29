bbfe93cf701c3d984106e3dcc4d4567bd43af227
Francesc Esplugas <fesplugas@intraducibles.net>
2008-03-29 22:27:56 +0100

    Added string test

---
+++ test/lib/string_test.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class StringTest < Test::Unit::TestCase
+
+  def test_should_return_sql_conditions_on_search_for_typus_user
+    query = "search=francesc"
+    processed = query.build_conditions(TypusUser)
+    expected = "AND (LOWER(first_name) LIKE '%francesc%' OR LOWER(last_name) LIKE '%francesc%' OR LOWER(email) LIKE '%francesc%') "
+    assert_equal processed, expected
+  end
+  
+  def test_should_return_sql_conditions_on_search_and_filter_for_typus_user
+    query = "search=francesc&status=true"
+    processed = query.build_conditions(TypusUser)
+    expected = "AND (LOWER(first_name) LIKE '%francesc%' OR LOWER(last_name) LIKE '%francesc%' OR LOWER(email) LIKE '%francesc%') AND status = 't' "
+    assert_equal processed, expected
+  end
+
+  def test_should_return_sql_conditions_on_search_for_post
+    query = "search=pum"
+    processed = query.build_conditions(Post)
+    expected = "AND (LOWER(title) LIKE '%pum%') "
+    assert_equal processed, expected
+  end
+
+end
