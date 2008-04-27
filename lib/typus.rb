364615a5f857741e7e9c7ae3262e7b09f57b7269
Francesc Esplugas <fesplugas@intraducibles.net>
2008-04-27 21:59:45 +0200

    Added new methods to get app names, modules ...

--- lib/typus.rb
+++ lib/typus.rb
@@ -2,6 +2,42 @@ module Typus
 
   class << self
 
+    def apps
+      modules = []
+      Typus::Configuration.config.to_a.each do |model|
+        modules << ((model[1].has_key? 'module') ? model[1]['module'] : 'Typus')
+      end
+      return modules.uniq
+    end
+
+    def modules(app_name)
+      submodules = []
+      Typus::Configuration.config.to_a.each do |model|
+        if model[1]['module'] == app_name
+          submodules << model[0]
+        end
+      end
+      return submodules
+    end
+
+    def submodules(module_name)
+      submodules = []
+      Typus::Configuration.config.to_a.each do |model|
+        if model[1]['submodule'] == module_name
+          submodules << model[0]
+        end
+      end
+      return submodules
+    end
+
+    def parent_module(submodule_name)
+      # TODO
+    end
+
+    def parent_app(module_name)
+      # TODO
+    end
+
     def enable
       enable_configuration
       enable_orm
