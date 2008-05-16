2ade298197faaeb052d9fd3fdb429e11ca89f919
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-16 10:46:47 +0200

    Small fixes. Currently breaks the order of the attributes

--- lib/typus/active_record.rb
+++ lib/typus/active_record.rb
@@ -15,7 +15,7 @@ module Typus
     end
 
     def self.model_fields
-      self.columns.map { |u| ["#{u.name}", "#{u.type}"] }
+      self.columns.map { |u| [u.name, u.type.to_s] }
     end
 
     # Form and list fields
@@ -24,45 +24,38 @@ module Typus
     #   typus_list_fields :name, :created_at, :updated_at, :status
     #   typus_form_fields :name, :body, :excerpt, :created_at
     #
-    def self.typus_fields_for(filter)
+    def self.typus_fields_for(filter = 'list')
 
       available_fields = self.model_fields
 
-      if Typus::Configuration.config["#{self.to_s.titleize}"]["fields"].has_key? filter
-        fields = Typus::Configuration.config["#{self.to_s.titleize}"]["fields"][filter]
-        fields = (fields.nil?) ? available_fields : fields.split(", ")
-      else
-        fields = available_fields
-      end
-
-      fields_with_type = []
-      fields.each do |f|
-
-        ##
-        # Get the field_type for each field
-        available_fields.each do |af|
-          @field_type = af.last if af.first == f
-        end
+      available_fields.each do |f|
 
         ##
         # Some custom field_type depending on the attribute name
-        case f
-          when /_id/:             @field_type = 'collection'
-          when /file_name/:       @field_type = 'file'
-          when /password/:        @field_type = 'password'
-          when 'parent_id':       @field_type = 'tree'
-          when 'position':        @field_type = 'position'
-          else @field_type = 'string' if @field_type == ""
+        case f.first
+          when /_id/:             f[1] = 'collection'
+          when /file_name/:       f[1] = 'file'
+          when /password/:        f[1] = 'password'
+          when 'parent_id':       f[1] = 'tree'
+          when 'position':        f[1] = 'position'
         end
-
-        # @field_type = (eval f.upcase) rescue @field_type
-        @field_type = 'selector' if @field_type.class == Array
-        fields_with_type << [ f, @field_type ]
-        @field_type = ""
 
       end
 
-      return fields_with_type
+      begin
+        Typus::Configuration.config["#{self.to_s.titleize}"]["fields"].has_key? filter
+        fields = Typus::Configuration.config["#{self.to_s.titleize}"]["fields"][filter]
+        custom_available_fields = []
+        #fields.each do |f|
+        #  custom_available_fields << [f, ]
+        #end
+        available_fields.each do |af|
+          custom_available_fields << af if fields.include? af.first
+        end
+        return custom_available_fields
+      rescue
+        return available_fields
+      end
 
     end
 
@@ -75,14 +68,9 @@ module Typus
     def self.typus_filters
       available_fields = self.model_fields
       fields = Typus::Configuration.config["#{self.to_s.titleize}"]["filters"].split(", ")
-      fields_with_type = Array.new
-      fields.each do |f|
-        available_fields.each do |af|
-          @field_type = af[1] if af[0] == f
-        end
-        fields_with_type << [ f, @field_type ]
-      end
-      return fields_with_type
+      custom_available_fields = []
+      available_fields.each { |af| custom_available_fields << af if fields.include? af.first }
+      custom_available_fields
     rescue
       []
     end
@@ -94,22 +82,24 @@ module Typus
     #     typus_list_actions :action_one
     #     typus_form_actions :action_two, :action_three
 
-    def self.typus_actions_for(filter)
-      Typus::Configuration.config["#{self.to_s.titleize}"]["actions"][filter].split(", ") rescue []
+    def self.typus_actions_for(filter = "list")
+      Typus::Configuration.config["#{self.to_s.titleize}"]["actions"][filter].split(", ")
+    rescue
+      []
     end
 
     ##
-    # Used for +order_by+, +search+ and more ...
+    # Used for +search+
     #
     # Someday we could use something like:
     #     typus_search :title, :details
-    #     typus_related :tags, :categories
-    #     typus_order_by :title, :created_at
     #
     # Default order is ASC, except for datetime items that is DESC.
 
     def self.typus_defaults_for(filter)
-      Typus::Configuration.config["#{self.to_s.titleize}"][filter].split(", ") rescue []
+      Typus::Configuration.config["#{self.to_s.titleize}"][filter].split(", ")
+    rescue
+      []
     end
 
     ##
@@ -122,10 +112,16 @@ module Typus
         self.reflections.each do |name, value|
           associations << name.to_s if value.macro.to_s == filter
         end
-        return associations
+        associations
       end
     end
 
+    ##
+    # Used for +order_by+
+    #
+    # Someday we could use something like:
+    #     typus_order_by :title, :created_at
+    #
     def self.typus_order_by
       fields = Typus::Configuration.config["#{self.to_s.titleize}"]["order_by"].split(", ")
       order = []
@@ -156,7 +152,9 @@ module Typus
     end
 
     def typus_name
-      name rescue "#{self.class}##{id}"
+      name
+    rescue
+      "#{self.class}##{id}"
     end
 
   end
