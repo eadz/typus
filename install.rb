f870d0178fd8762d7b380773b989cb0907edce83
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-21 17:28:11 +0200

    Cleaned

--- install.rb
+++ install.rb
@@ -1,6 +1,1 @@
-%w( stylesheets images ).each do |f|
-  system "cp #{File.dirname(__FILE__)}/public/#{f}/* #{File.dirname(__FILE__)}/../../../public/#{f}/"
-end
-
-puts "[Typus] Available Tasks"
 system "rake typus"
