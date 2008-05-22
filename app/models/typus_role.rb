5fd41cb2148304a6883bda913793c0f4fb0940fb
Francesc Esplugas <fesplugas@intraducibles.net>
2008-05-22 06:57:35 +0200

    Updated migration and added model

---
+++ app/models/typus_role.rb
@@ -0,0 +1,5 @@
+class TypusRole < ActiveRecord::Base
+
+  validates_presence_of :name
+
+end
--- generators/typus_migration/templates/migration.rb
+++ generators/typus_migration/templates/migration.rb
@@ -1,6 +1,7 @@
 class CreateTypusUsers < ActiveRecord::Migration
 
   def self.up
+
     create_table :typus_users do |t|
       t.string :email, :salt, :crypted_password
       t.string :first_name, :last_name
@@ -8,10 +9,22 @@ class CreateTypusUsers < ActiveRecord::Migration
       t.boolean :admin, :default => false
       t.timestamps
     end
+
+    create_table :typus_roles do |t|
+      t.string :name, :email
+      t.string :first_name, :last_name
+      t.boolean :is_superuser, :default => false
+      t.boolean :can_create_rol, :default => false
+      t.boolean :can_login, :default => false
+      t.string :salt, :crypted_password
+      t.timestamp :valid_until
+    end
+
   end
 
   def self.down
     drop_table :typus_users
+    drop_table :typus_roles
   end
 
 end
